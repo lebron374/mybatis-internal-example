9c8d2d60783a84efa0b55f6914b60acd0ae797f4
virgil <17816853545@163.com>
2020-05-09 14:29:29 +0800

    dev

---
+++ src/main/java/cn/edu/example/reflect/GenericReflectionTest.java
@@ -0,0 +1,38 @@
+package cn.edu.example.reflect;
+
+import java.lang.reflect.Method;
+import java.util.Scanner;
+
+/**
+ * @author virgil.Wang
+ * @date 2020/5/8 18:40
+ */
+public class GenericReflectionTest {
+
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            try(Scanner in = new Scanner(System.in)){
+                System.out.println("Enter class name (e.g. java.util.Collections)");
+                name = in.next();
+            }
+        }
+
+        try {
+            Class<?> cl = Class.forName(name);
+            for (Method m : cl.getDeclaredMethods()) {
+
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printClass(Class<?> cl) {
+        System.out.println(cl);
+
+    }
+
+}
